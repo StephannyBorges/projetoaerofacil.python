968e1c98241a5b242c075f36c2649589c8a41469
Stephanny <stephannyborgestete@gmail.com>
2025-11-24 08:30:17 -0300

    ajuste2

--- aerofacil_sistema/aerofacil_sistema/settings.py
+++ aerofacil_sistema/aerofacil_sistema/settings.py
@@ -112,6 +112,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Adicione estas linhas:
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
