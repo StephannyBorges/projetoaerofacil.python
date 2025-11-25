8c887fae37a3aa56afdef3e309b9149bbfc385f7
Stephanny <stephannyborgestete@gmail.com>
2025-11-25 10:21:50 -0300

    pagina de confirmação

--- aerofacil_sistema/aerofacil_sistema/urls.py
+++ aerofacil_sistema/aerofacil_sistema/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import login_view
+from core import views  # <--- ESSA LINHA ERA A QUE FALTAVA!
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', login_view, name='login'),
+    
+    # Rota para a página de Login/Busca (Página inicial)
+    path('', views.login_view, name='login'),
+
+    # Rota para a página de Confirmação
+    path('confirmacao/', views.confirmacao_view, name='confirmacao'),
 ]
--- aerofacil_sistema/core/views.py
+++ aerofacil_sistema/core/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def login_view(request):
     return render(request, 'login.html')
+from django.shortcuts import render
+# --- ADICIONE ESSA NOVA PARTE ---
+def confirmacao_view(request):
+    # Aqui futuramente você pode pegar os dados do formulário
+    return render(request, 'confirmacao.html')
